b0da1bade34faf7290cbc0177826b651074f9b50
sauhsalom <160551031+sauhsalom@users.noreply.github.com>
2024-02-20 15:26:12 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        static void Calculadora()
+        {
+            int valor1;
+            int valor2;
+
+            Console.WriteLine("bem vindo a minha calculadora");
+            Console.WriteLine("______________________________________");
+            Console.WriteLine("por gentileza digite abaixo o primeiro valor");
+
+            valor1 = Convert.ToInt32(Console.ReadLine());
+
+            valor2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("bem vindo a minha calculadora");
+            Console.WriteLine("______________________________________");
+            Console.WriteLine("por gentileza digite abaixo o primeiro valor");
+
+        }
+    }
+}
